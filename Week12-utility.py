01a89ca0760c6e424ad7fc1292f6247e255e5da4
MinesASME <MinesASME@users.noreply.github.com>
2019-11-15 21:30:46 -0700

    4. FindWordCount

--- Week12-utility.py
+++ Week12-utility.py
@@ -5,7 +5,7 @@
 
 def PrintOutput(inputString):
     print("OUTPUT %s" %inputString)
-########################################
+############################################################################
 def LoadFile(inputString):
     contents = []
     with open(inputString, 'r') as file:
@@ -14,8 +14,15 @@ def LoadFile(inputString):
             current = line[:-1]
             contents.append(current)
     return contents
-#######################################
+############################################################################
 def UpdateString(word, insert, index):
     firstHalf = word[:index]
     secondHalf = word[index+1:]
     PrintOutput(firstHalf+insert+secondHalf)
+############################################################################
+def FindWordCount(wordList,wordSearch):
+    count = 0
+    for i in range(0,len(wordList)):
+        count = count + wordList[i].count(wordSearch)
+    return count
+############################################################################
