65b5b76677184ed7627913382f5a39a65d7e8be2
MinesASME <MinesASME@users.noreply.github.com>
2019-11-15 21:03:11 -0700

    2. LoadFile

--- Week12-utility.py
+++ Week12-utility.py
@@ -6,3 +6,11 @@
 def PrintOutput(inputString):
     print("OUTPUT %s" %inputString)
 
+def LoadFile(inputString):
+    contents = []
+    with open(inputString, 'r') as file:
+        filecontents = file.readlines()
+        for line in filecontents:
+            current = line[:-1]
+            contents.append(current)
+    return contents
